6ab1e4ce602926fd798c766bcc4e77d9e69249fb
Yusong <1138596129@qq.com>
2023-12-13 22:16:07 +0800

    2023.12.13 22:16

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      //JSX 能够像写html一样写页面
+      <div className="box">
+        <h1>Hello</h1>
+        <button onClick={() => {alert("Hello")}}>按钮</button>
+      </div>
   );
 }
 
